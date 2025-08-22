113dc2a8b61e78e0dcfe8eb46220e71fbbe8e654
meetcollege <meetn0101@gmail.com>
2025-08-22 14:11:35 +0530

    salary

---
+++ shape/shape/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="shape.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbl_length" runat="server" Text="Length for Square:"></asp:Label>
+            <asp:TextBox ID="txt_length" runat="server"></asp:TextBox><br />
+            <asp:Label ID="lbl_radius" runat="server" Text="Radius for Circle:"></asp:Label>
+            <asp:TextBox ID="txt_radius" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Button ID="claculate" runat="server" Text="Calculate" OnClick="claculate_Click" /><br /><br />
+
+            <asp:Label ID="lbl_outputtxt" runat="server" Text="Output: "></asp:Label>
+            <asp:Label ID="lbl_output" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ shape/shape/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace shape
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        class Shape
+        {
+            public String name;
+
+            public Shape()
+            {
+                name = "";
+            }
+            public Shape(String name)
+            {
+                this.name = name;
+            }
+            public String Display()
+            {
+                return name;
+            }
+        }
+        
+        class Square : Shape
+        {
+            public int length;
+            public Square() { 
+                length = 5;
+            }
+            public Square(int length, String name) { 
+                this.length = length;
+                this.name = name;
+            }
+            public int Calculate_perimeter()
+            {
+                return 4*length;
+            }
+        }
+
+        class Circle : Shape
+        {
+            public int radius;
+            public Circle() {
+                radius = 5;
+            }
+            public Circle(int radius, String name) { 
+                this.radius = radius;
+                this.name = name;
+            }
+
+            public double Calculate_perimeter() {
+                return 2 * 3.14 * radius;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void claculate_Click(object sender, EventArgs e)
+        {
+
+            if(txt_length.Text != "" && txt_radius.Text != "")
+            {
+                int length = int.Parse(txt_length.Text);
+                int radius = int.Parse(txt_radius.Text);
+
+                Square s1 = new Square(length,"Square");
+                Circle c1 = new Circle(radius,"Circle");
+
+                lbl_output.Text = "Name: " + s1.Display() + "<br/>Perimeter: " + s1.Calculate_perimeter() + "<br/><br/>Name: " + c1.Display() + "<br/>Perimeter: " + c1.Calculate_perimeter();
+            }
+        }
+    }
+}
